6979f07339ce3e3f9c06f91fefc7dc1c99516a23
tylermorganwall <tylermorganwall@users.noreply.github.com>
2023-12-01 22:05:16 -0500

    rayvertex v0.10.1: Fix UV mapping function

--- R/add_plane_uv_mesh.R
+++ R/add_plane_uv_mesh.R
@@ -1,13 +1,14 @@
 #' @title Add Plane UV Mapping to Mesh
 #'
-#' @description Applies a planar UV mapping to a mesh based on a given direction. 
+#' @description Applies a planar UV mapping to a mesh based on a given direction and set of U/V vectors.
 #' If `full_mesh_bbox` is true, the UV mapping is scaled based on the bounding box 
-#' of the entire mesh. If false, each shape's bounding box is used.
+#' of the entire mesh. If false, each shape's bounding box is used. One of direction/u/v must be `NULL` and will be calculated from the others.
 #'
 #' @param mesh The mesh to which the UV mapping will be applied.
 #' @param direction Default `c(0, 1, 0)`. A vector specifying the direction for 
 #' UV mapping. The function normalizes this vector.
-#' @param up Default `c(0, 1, 0)`. A vector specifying the up direction.
+#' @param u Default `NULL`. A vector specifying the u direction. 
+#' @param v Default `NULL`. A vector specifying the v direction.
 #' @param override_existing Default `FALSE`. Specifies whether existing UV 
 #' coordinates should be overridden.
 #' @param full_mesh_bbox Default `TRUE`. Specifies whether the full mesh's 
@@ -17,13 +18,13 @@
 #' @export
 #' @examples
 #'if(rayvertex:::run_documentation()) {
-#'#Let's construct a mesh from the volcano dataset
+#' #Let's construct a mesh from the volcano dataset
 #' #Build the vertex matrix
 #' vertex_list = list()
 #' counter = 1
 #' for(i in 1:nrow(volcano)) {
 #'   for(j in 1:ncol(volcano)) {
-#'     vertex_list[[counter]] = matrix(c(j,volcano[i,j],i), ncol=3)
+#'     vertex_list[[counter]] = matrix(c(j,volcano[i,j]/3,i), ncol=3)
 #'     counter = counter + 1
 #'   }
 #' }
@@ -35,33 +36,90 @@
 #' for(i in 1:(nrow(volcano)-1)) {
 #'   for(j in 1:(ncol(volcano)-1)) {
 #'     index_list[[counter+1]] = matrix(c(counter,counter+ncol(volcano),counter+1,
-#'                                        counter+ncol(volcano),counter+ncol(volcano)+1,counter + 1), 
+#'                                        counter+ncol(volcano),counter+ncol(volcano)+1,counter + 1),
 #'                                      nrow=2, ncol=3, byrow=TRUE)
 #'     counter = counter + 1
 #'   }
 #'   counter = counter + 1
 #' }
-#' indices = do.call(rbind,index_list)
+#' indices = do.call("rbind",index_list)
+#' 
+#' #Create a checkerboard image
+#' create_checkerboard_texture = function(filename, n = 16) {
+#'   old_par = par(no.readonly = TRUE)
+#'   on.exit(par(old_par))
+#'   plot.new()
+#'   par(mar = c(0, 0, 0, 0))
+#'   checkerboard = matrix(c(1, 0), nrow = n+1, ncol = n)
+#'   png(filename, width = 800, height = 800)
+#'   image(1:(n+1), 1:n, checkerboard, col = c("dodgerblue", "red"),
+#'         axes = FALSE, xlab = "", ylab = "")
+#'   dev.off()
+#' }
+#' checkerboard_file = tempfile(fileext = ".png")
+#' create_checkerboard_texture(checkerboard_file)
 #' 
 #' #Construct the mesh
 #' volc_mesh = construct_mesh(vertices = vertices, indices = indices,
-#'                            material = material_list(type="phong", diffuse="darkred", 
+#'                            material = material_list(type="phong", diffuse="darkred",
 #'                                                     ambient = "darkred", ambient_intensity=0.2))
-#' add_plane_uv_mesh(volc_mesh, direction=c(-50,230,100))->uv
-#' uv = set_material(uv, texture_location = "~/Desktop/diamondcali.png")
+#' 
+#' 
+#' #Set the direction so that the checkerboard will be mapped to the surface like a carpet
+#' uv = add_plane_uv_mesh(volc_mesh, direction=c(0,200,0), u = c(1,0,0))
+#' uv = set_material(uv, texture_location = checkerboard_file,
+#'                   ambient = "white", ambient_intensity=0.1)
 #' #Rasterize the scene
-#' rasterize_scene(uv, lookfrom=c(-50,230,100),fov=0,width=1200,height=1200,
+#' rasterize_scene(center_mesh(uv), lookfrom=c(200,200,200),fov=0,width=1200,height=1200,
 #'                 light_info = directional_light(c(0,1,1)) |>
-#'                   add_light(directional_light(c(1,1,-1))),ortho_dimensions=c(200,200))
+#'                   add_light(directional_light(c(1,1,-1))),ortho_dimensions=c(120,120))
+#' 
+#' 
+#' #Set the direction so that the checkerboard will be mapped directly at the camera
+#' uv = add_plane_uv_mesh(volc_mesh, direction=c(200,200,200), v = c(-1,1,-1))
+#' uv = set_material(uv, texture_location = checkerboard_file,
+#'                   ambient = "white", ambient_intensity=0.1)
+#' #Rasterize the scene
+#' rasterize_scene(center_mesh(uv), lookfrom=c(200,200,200),fov=0,width=1200,height=1200,
+#'                 light_info = directional_light(c(0,1,1)) |>
+#'                 add_light(directional_light(c(1,1,-1))), ortho_dimensions=c(120,120))
 #'}
-add_plane_uv_mesh = function(mesh, direction = c(0, 1, 0), up = c(0,1,0), 
+add_plane_uv_mesh = function(mesh, direction = c(0,1,0), u = NULL, v = NULL, 
                              override_existing = FALSE, full_mesh_bbox = TRUE) {
-  # Normalize the direction vector
-  direction = direction / sqrt(sum(direction^2))
+  duv = c(FALSE, FALSE, FALSE)
+  if(is.null(direction) && !is.null(u) && !is.null(v)) {
+    duv[1] = TRUE
+  } 
+  if(!is.null(direction) && is.null(u) && !is.null(v)) {
+    duv[2] = TRUE
+  }
+  if(!is.null(direction) && !is.null(u) && is.null(v)) {
+    duv[3] = TRUE
+  }
+  if(sum(duv) != 1) {
+    stop("Exactly one of `direction`/`u`/`v` must be NULL")
+  }
+  if(duv[1]) {
+    u = u / sqrt(sum(u^2))
+    v = v / sqrt(sum(v^2))
+    direction = cross_prod(u,v)
+  } else if (duv[2]) {
+    direction = direction / sqrt(sum(direction^2))
+    v = v / sqrt(sum(v^2))
+    u = cross_prod(direction,v)
+  } else {
+    direction = direction / sqrt(sum(direction^2))
+    u = u / sqrt(sum(u^2))
+    v = cross_prod(u,direction)
+  }
+  
+  rot_mat = matrix(c(u,direction,v),nrow=3,ncol=3, byrow=TRUE)
+  
   if(full_mesh_bbox) {
-    bbox_val = get_mesh_bbox(mesh)
-    xrange = range(bbox_val[,1])
-    zrange = range(bbox_val[,3])
+    all_verts = do.call("rbind", mesh$vertices)
+    all_verts = t(apply(all_verts,1,\(x) rot_mat %*% x))
+    xrange = range(all_verts[,1])
+    zrange = range(all_verts[,3])
     stopifnot(xrange[1] != xrange[2] && zrange[1] != zrange[2])
   }
   # Iterate over shapes
@@ -72,23 +130,20 @@ add_plane_uv_mesh = function(mesh, direction = c(0, 1, 0), up = c(0,1,0),
     if (!override_existing && any(shape$has_vertex_tex)) next
     
     vertices_bbox = mesh$vertices[[i]]
-    if(!all(direction == up)) {
-      rot_mat = generate_rotation_matrix_from_direction(direction, up)
-      vertices_bbox = t(apply(vertices_bbox,1,\(x) x %*% rot_mat))
-    }
+    vertices_bbox = t(apply(vertices_bbox,1,\(x) rot_mat %*% x))
+    
     if(!full_mesh_bbox) {
       xrange = range(vertices_bbox[,1])
       zrange = range(vertices_bbox[,3])
       stopifnot(xrange[1] != xrange[2] && zrange[1] != zrange[2])
     }
-    
 
     vertices_bbox[,1] = (vertices_bbox[,1] - xrange[1])/(xrange[2]- xrange[1])
     vertices_bbox[,3] = (vertices_bbox[,3] - zrange[1])/(zrange[2]- zrange[1])
     
     # Initialize a matrix to store UV coordinates
-    uv_coords = vertices_bbox[,c(1,3)]
-    
+    uv_coords = vertices_bbox[,c(3,1)]
+
     # Assign the UV coords to the shape
     mesh$texcoords[[i]] = uv_coords
     mesh$shapes[[i]]$tex_indices = mesh$shapes[[i]]$indices
@@ -127,8 +182,9 @@ cross_prod = function(x,y) {
 #'
 #' @keywords internal
 generate_rotation_matrix_from_direction = function(direction = c(0, 1, 0), up = c(0, 1, 0)) {
-  # Ensure the direction vector is a unit vector
+  # Ensure the vectors are unit vectors
   direction = direction / sqrt(sum(direction^2))
+  up = up / sqrt(sum(direction^2))
   
   # The rotation axis is the cross product of the direction vector and the y-axis
   rotation_axis = cross_prod(matrix(direction, ncol = 3), matrix(up, ncol = 3))
@@ -154,5 +210,5 @@ generate_rotation_matrix_from_direction = function(direction = c(0, 1, 0), up = 
     uz * ux * (1 - cos_angle) - uy * sin_angle, uz * uy * (1 - cos_angle) + ux * sin_angle, cos_angle + uz^2 * (1 - cos_angle)
   ), nrow = 3, byrow = TRUE)
   
-  return(R)
+  return(t(R))
 }
